9fa39723614912975265b64e51a6d532f2e32cc8
plestau <pablitolestau@hotmail.com>
2023-10-27 19:42:49 +0200

    Ejercicio 1 mutado con lista de planetas

--- src/Ejercicio1Mutado/Main.kt
+++ src/Ejercicio1Mutado/Main.kt
@@ -1,2 +1,42 @@
 package Ejercicio1Mutado
 
+fun main() {
+    val mercurio = Planeta("Mercurio", "Rocoso", 3.3011E23)
+    val venus = Planeta("Venus", "Rocoso", 4.8675E24)
+    val tierra = Planeta("Tierra", "Rocoso", 5.972E24)
+    val marte = Planeta("Marte", "Rocoso", 6.39E23)
+    val jupiter = Planeta("Jupiter", "Gaseoso", 1.898E27)
+    val saturno = Planeta("Saturno", "Gaseoso", 5.683E26)
+    val urano = Planeta("Urano", "Gaseoso", 8.681E25)
+    val neptuno = Planeta("Neptuno", "Gaseoso", 1.024E26)
+    val planetas = listOf(mercurio, venus, tierra, marte, jupiter, saturno, urano, neptuno)
+    println("El planeta con mayor masa es ${tierra.planetaMayor(planetas).nombre}")
+    println("El planeta con menor masa es ${tierra.planetaMenor(planetas).nombre}")
+    println("La masa media de los planetas es ${tierra.masaMedia(planetas)}")
+}
+
+class Planeta(val nombre: String, val tipo: String, val masa: Double){
+    fun planetaMayor(planetas: List<Planeta>): Planeta{
+        val planetasOrdenados = planetas.sortedBy { this.masa }
+        val planetaMayor = planetasOrdenados[planetasOrdenados.size - 1]
+        return planetaMayor
+    }
+
+    fun planetaMenor(planetas: List<Planeta>): Planeta{
+        // orderna los planetas por orden de masa pasa sacar el que menos masa tiene
+        val planetasOrdenados = planetas.sortedBy { this.masa }
+        val planetaMenor = planetasOrdenados[0]
+        return planetaMenor
+    }
+
+    fun masaMedia(planetas: List<Planeta>): Double{
+        var masaTotal = 0.0
+        var media = 0.0
+        for (planeta in planetas) {
+            masaTotal += planeta.masa
+            media = masaTotal / planetas.size
+        }
+        return media
+    }
+}
+
--- src/Ejercicio2Mutado/Main.kt
+++ src/Ejercicio2Mutado/Main.kt
@@ -1,2 +1,8 @@
 package Ejercicio2Mutado
 
+fun main(){
+
+}
+
+
+class Triangulo(val l_lado1: Double, val l_lado2: Double, val l_lado3: Double)
