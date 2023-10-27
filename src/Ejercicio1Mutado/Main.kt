7923867f822cc07a45b9e270c14c499d6fc87674
plestau <pablitolestau@hotmail.com>
2023-10-27 20:18:31 +0200

    subiendo ejercicios mutados

--- src/Ejercicio1Mutado/Main.kt
+++ src/Ejercicio1Mutado/Main.kt
@@ -3,19 +3,25 @@ package Ejercicio1Mutado
 fun main() {
     val mercurio = Planeta("Mercurio", "Rocoso", 3.3011E23)
     val venus = Planeta("Venus", "Rocoso", 4.8675E24)
-    val tierra = Planeta("Tierra", "Rocoso", 5.972E24)
+    val tierra = Planeta("Tierra", "Habitable", 5.972E24)
     val marte = Planeta("Marte", "Rocoso", 6.39E23)
     val jupiter = Planeta("Jupiter", "Gaseoso", 1.898E27)
     val saturno = Planeta("Saturno", "Gaseoso", 5.683E26)
     val urano = Planeta("Urano", "Gaseoso", 8.681E25)
     val neptuno = Planeta("Neptuno", "Gaseoso", 1.024E26)
     val planetas = listOf(mercurio, venus, tierra, marte, jupiter, saturno, urano, neptuno)
-    println("El planeta con mayor masa es ${tierra.planetaMayor(planetas).nombre}")
-    println("El planeta con menor masa es ${tierra.planetaMenor(planetas).nombre}")
-    println("La masa media de los planetas es ${tierra.masaMedia(planetas)}")
+    println("El planeta con mayor masa es: ${mercurio.planetaMayor(planetas).nombre} con una masa de ${venus.planetaMayor(planetas).masa} kg")
+    println("El planeta con menor masa es: ${tierra.planetaMenor(planetas).nombre} con una masa de ${marte.planetaMenor(planetas).masa} kg")
+    println("La masa media de los planetas es: ${jupiter.masaMedia(planetas)} kg")
 }
 
-class Planeta(val nombre: String, val tipo: String, val masa: Double){
+class Planeta(val nombre: String, var tipo: String, val masa: Double){
+    init {
+        if (tipo != "Rocoso" && tipo != "Gaseoso") {
+            // throw Exception("El planeta $nombre no es de tipo 'Rocoso' o 'Gaseoso'")
+            this.tipo = "Rocoso"
+        }
+    }
     fun planetaMayor(planetas: List<Planeta>): Planeta{
         val planetasOrdenados = planetas.sortedBy { this.masa }
         val planetaMayor = planetasOrdenados[planetasOrdenados.size - 1]
@@ -23,7 +29,6 @@ class Planeta(val nombre: String, val tipo: String, val masa: Double){
     }
 
     fun planetaMenor(planetas: List<Planeta>): Planeta{
-        // orderna los planetas por orden de masa pasa sacar el que menos masa tiene
         val planetasOrdenados = planetas.sortedBy { this.masa }
         val planetaMenor = planetasOrdenados[0]
         return planetaMenor
@@ -37,6 +42,18 @@ class Planeta(val nombre: String, val tipo: String, val masa: Double){
             media = masaTotal / planetas.size
         }
         return media
+    }
+
+    fun verMasa(): Double {
+        return masa
+    }
+
+    fun verTipo(): String {
+        return tipo
+    }
+
+    fun verNombre(): String {
+        return nombre
     }
 }
 
--- src/Ejercicio2Mutado/Main.kt
+++ src/Ejercicio2Mutado/Main.kt
@@ -1,8 +1,60 @@
 package Ejercicio2Mutado
 
 fun main(){
-
+    var rectangulo = Triangulo(5.0, 3.0, 4.0)
+    var equilatero = Triangulo(3.0, 3.0, 3.0)
+    var isosceles = Triangulo(3.0, 3.0, 4.0)
+    var escaleno = Triangulo(3.0, 4.0, 5.0)
+    println(rectangulo)
+    println(equilatero)
+    println(isosceles)
+    println(escaleno)
 }
 
 
-class Triangulo(val l_lado1: Double, val l_lado2: Double, val l_lado3: Double)
+class Triangulo{
+    var hipotenusa: Double
+    var l_lado1: Double
+    var l_lado2: Double
+    var perimetro: Double
+    var tipo: String
+    var esRectangulo: Boolean
+
+    constructor(hipotenusa: Double, lado1: Double, lado2: Double){
+        this.hipotenusa = hipotenusa
+        this.l_lado1 = lado1
+        this.l_lado2 = lado2
+        this.perimetro = hipotenusa + lado1 + lado2
+        if(hipotenusa == lado1 && lado1 == lado2){
+            this.tipo = "Equilatero"
+        }else if(hipotenusa == lado1 || lado1 == lado2 || hipotenusa == lado2){
+            this.tipo = "Isosceles"
+        }
+        else{
+            this.tipo = "Escaleno"
+        }
+        if(hipotenusa == Math.sqrt(Math.pow(lado1, 2.0) + Math.pow(lado2, 2.0))) {
+            this.esRectangulo = true
+        }else{
+            this.esRectangulo = false
+        }
+    }
+    fun calculaArea(): Double{
+        return (this.l_lado1 * this.l_lado2) / 2
+    }
+    fun calculaAltura(): Double{
+        return (this.l_lado1 * this.l_lado2) / this.hipotenusa
+    }
+
+    override fun toString(): String {
+        return "Progama analizador de triángulos, con los datos: \n" +
+                "Lado 1: ${this.hipotenusa} \n" +
+                "Lado 2: ${this.l_lado1} \n" +
+                "Lado 3: ${this.l_lado2} \n" +
+                "Perímetro: ${this.perimetro} \n" +
+                "Tipo: ${this.tipo} \n" +
+                "Es rectángulo: ${this.esRectangulo} \n" +
+                "Área: ${this.calculaArea()} \n" +
+                "Altura: ${this.calculaAltura()} \n"
+    }
+}
